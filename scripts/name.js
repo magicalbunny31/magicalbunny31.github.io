1e9ab5a882d8f3e1a73bbc6573d0d9f1951744b4
magicalbunny31 <dzeiun@googlemail.com>
2022-01-17 00:24:04 +0000

    add current files 🐾

---
+++ scripts/name.js
@@ -0,0 +1,71 @@
+const month = new Date().getMonth();
+let magicalbunny31 = `magicalbunny31`;
+
+
+const matchesNumber = (number, ...numbers) => numbers.includes(number);
+
+
+// seasonal emojis
+if (matchesNumber(month, 11,  0,  1)) // winter
+  magicalbunny31 += ` ❄`;
+if (matchesNumber(month,  2,  3,  4)) // spring
+  magicalbunny31 += ` 🍃`;
+if (matchesNumber(month,  5,  6,  7)) // summer
+  magicalbunny31 += ` ☀`;
+if (matchesNumber(month,  8,  9, 10)) // autumn
+  magicalbunny31 += ` 🍂`;
+
+
+// month emojis
+switch (month) {
+  case 0: // january
+    magicalbunny31 += `🎆`;
+    break;
+
+  case 1: // february
+    magicalbunny31 += `🌸`;
+    break;
+
+  case 2: // march
+    magicalbunny31 += `🍰`;
+    break;
+
+  case 3: // april
+    magicalbunny31 += `🌧`;
+    break;
+
+  case 4: // may
+    magicalbunny31 += `🌺`;
+    break;
+
+  case 5: // june
+    magicalbunny31 += `🌻`;
+    break;
+
+  case 6: // july
+    magicalbunny31 += `🏖`;
+    break;
+
+  case 7: // august
+    magicalbunny31 += `🌇`;
+    break;
+
+  case 8: // september
+    magicalbunny31 += `🌃`;
+    break;
+
+  case 9: // october
+    magicalbunny31 += `🎃`;
+    break;
+
+  case 10: // november
+    magicalbunny31 += `☕`;
+    break;
+
+  case 11: // december
+    magicalbunny31 += `🎄`;
+    break;
+};
+
+
+document.title = magicalbunny31;
