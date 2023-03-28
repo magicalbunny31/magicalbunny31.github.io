987d43b296e98f33f26f45a86f8565611579cb37
magicalbunny31 <63871095+magicalbunny31@users.noreply.github.com>
2023-03-28 23:47:52 +0100

    cutie 🐾

---
+++ assets/scripts/you-are-a-cutie.js
@@ -0,0 +1,16 @@
+// logs stuff to the console
+const cutie = `
+
+
+"hey cutie~"
+"no, no, you really are cute uwu"
+"don't deny it~"
+"i would even boop you if i could!"
+"but i can't, this is all just a computer simulation.."
+"'neways, see ya you adorable fur~"
+
+
+`.trim();
+
+
+console.info(cutie);
